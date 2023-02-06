8c8f4ed621cada74a1b87a90e5af7ec7a36b8c76
Kassito <98308251+Kassito@users.noreply.github.com>
2023-02-06 10:24:11 +0100

    mostrar

---
+++ js/crud.js
@@ -0,0 +1,59 @@
+
+    // const requestURL = "http://localhost:3000/invitados";
+    // const request = new XMLHttpRequest();
+
+    // request.open('GET', requestURL);
+    // request.responseType = 'json';
+    // request.send();
+// console.log(JSON.stringify(request));
+
+// if (document.readyState == 'loading') {
+//     document.addEventListener('DOMContentLoaded', mostrar);
+// }
+
+const promesa = mostrar();
+
+promesa
+.then(function imprimirPosts(json) {
+    const listaInvitados = JSON.parse(json);
+    listaInvitados.forEach(invitado => createLI(invitado.nombre, invitado.confirmado))
+    console.log('ok')
+  })
+ // .then(ul.appendChild(li))
+  .catch(function handleErrors(error) {
+    console.log('Error: ', error)
+  })
+
+function crear(invitado) {
+    
+}
+
+function mostrar() {
+    const requestURL = "http://localhost:3000/invitados";
+    return new Promise (function (resolve, reject){
+        const xhr = new XMLHttpRequest();
+        xhr.timeout = 3000;
+        xhr.onreadystatechange = function(e){
+            if(xhr.readyState === 4){
+                if(xhr.status === 200){
+                    resolve(xhr.response);
+                } else{
+                    reject(xhr.status);
+                }
+            }
+        };
+        xhr.ontimeout = function(){
+            reject("timeout");
+        };
+        xhr.open("GET", requestURL, true);
+        xhr.send();
+    });
+}
+
+function actualizar(invitado) {
+    
+}
+
+function borrar(invitado) {
+    
+}
